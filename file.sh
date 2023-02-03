b0965b38b426923b7b6a5ee2f2a0e3bee6dee3ae
Dayo Adediran <dmercygui@gmail.com>
2023-02-03 16:57:58 -0500

    devOp

---
+++ file.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#testing
+
+company=MicchysAfricanCaribbeanMarket
+name=Dmercy
+
+echo "Fintec-master"
+echo "DevOp is good"
+echo "I will be rich in 2023"
+echo "Jesus is Lord"
+echo "Sincerely"
+echo "$company"
+echo "$name"
+date
+
+
+
+
